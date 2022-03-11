58dbdfeb4c4944c1d94ad5f048a600224048d3d3
Nashon Ernest <92583323+ernestnash@users.noreply.github.com>
2022-03-11 09:11:02 +0300

    another day....

--- 11-03-22/QuadraticEqtns.c
+++ 11-03-22/QuadraticEqtns.c
@@ -7,30 +7,30 @@
 #include <stdlib.h>
 
 int main(){
-	double a, b, c, discriminant, root1, root2, imgroot, realroot;
+	float a, b, c, discriminant, root1, root2, imgroot, realroot;
 	
 	printf("enter coefficients of quadratic equations: ");
-	scanf("lf% lf% lf%",&a,&b,&c);// getting up the values of the constatnts.
+	scanf("%f %f %f",&a,&b,&c);// getting up the values of the constatnts.
 	
 	discriminant = b * b - 4 * a * c;//getting up the discriminant in preparation for the real solutions.
 	
-	//conditions for real roots 
+	//conditions for real roots but different roots
 	if(discriminant>0){
 		root1 = -b + sqrt(discriminant)/(2 * a);
 		root2 = -b - sqrt(discriminant)/(2 * a);
 		printf("resuls in two roots that are:\n");
-		printf("root 1 = %lf",root1);
-		printf("root 2 = %lf",root2);
-
-	}else if(discriminant == 0){
+		printf("root 1 = %f",root1);
+		printf("root 2 = %f",root2);
+	}//conditions for real and equal roots	
+	else if(discriminant == 0){
 		root1 = root2 = -b/(2 * a);
 		
-		printf("root 1 = root 2 = %lf",root1);
-
-	}else {
+		printf("root 1 = root 2 = %f",root1);
+	}//if roots are not real
+	else if(discriminant<0){
 		realroot = -b/(2 * a);
 		imgroot = -b - sqrt(discriminant) / (2 * a);
-		printf("root1 = %lf and root 2 = %lf", realroot, imgroot);
+		printf("root1 = %f and root 2 = %f", realroot, imgroot);
 	}
 	return 0;
 }
